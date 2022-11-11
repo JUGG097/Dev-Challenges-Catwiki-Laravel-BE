590749d63164d3936e3050fc05ca6d43bc5cc26f
Adeoluwa Adeboye <adeneye97@yahoo.com>
2022-11-11 08:07:36 +0000

    :construction: Folder structure and health check endpoint

---
+++ app/Http/Controllers/CatWikiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\CatWikiService;
+use Illuminate\Support\Facades\Response;
+
+class CatWikiController extends Controller
+{
+
+
+    public function __construct(CatwikiService $catwikiService)
+    {
+        $this->catwikiService = $catwikiService;
+    }
+
+    public function healthCheck(Request $req)
+    {
+        return Response::json([
+            "success" => true
+        ], 200);
+    }
+
+    public function showTopTen(Request $req)
+    {
+    }
+
+    public function showCatDetails(Request $req, $pathVariable)
+    {
+    }
+
+    public function showCatPhotos(Request $req, $pathVariable)
+    {
+    }
+
+    public function showBreedList(Request $req)
+    {
+    }
+}
